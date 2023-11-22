1745d64ed9609bcd743c3cc1d83079ebc492020a
thalisu <thalisondmcc@gmail.com>
2023-11-22 15:57:46 -0300

    Ex 3.3 done

---
+++ Part 3/Ex 3.3/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+
+let phoneBook = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(phoneBook);
+});
+
+app.get("/info", (request, response) => {
+  response.send(`
+  <p>Phonebook has info for ${phoneBook.length} people</p>
+  <p>${new Date().toString()}</p>
+  `);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = phoneBook.find((person) => person.id === id);
+  person ? response.json(person) : response.status(404).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
