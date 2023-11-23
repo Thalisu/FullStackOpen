94dd831c839ee1880d97ac8aca3389f0a06b8615
thalisu <thalisondmcc@gmail.com>
2023-11-23 13:37:19 -0300

    Ex 3.10 done

--- Part 3/Ex 3.10/index.js
+++ Part 3/Ex 3.10/index.js
@@ -23,7 +23,7 @@ app.use(
   })
 );
 app.use(cors());
-app.use(express.static("dist"));
+app.use(express.static("build"));
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
