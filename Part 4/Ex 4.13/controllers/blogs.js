c6fac37f2d348300c6092d7da7fae6ae373037ae
thalisu <thalisondmcc@gmail.com>
2023-11-27 16:16:38 -0300

    Ex 4.13 done

---
+++ Part 4/Ex 4.13/controllers/blogs.js
@@ -0,0 +1,26 @@
+const blogRouter = require("express").Router();
+const Blog = require("../models/blog");
+
+blogRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs).end();
+});
+
+blogRouter.post("/", async (request, response) => {
+  const blog = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes,
+  });
+
+  const newBlog = await blog.save();
+  response.status(201).json(newBlog).end();
+});
+
+blogRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
+});
+
+module.exports = blogRouter;
