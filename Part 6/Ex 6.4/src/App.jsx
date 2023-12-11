138a11e4404aa46276a91165236906e4ec7a1c08
thalisu <thalisondmcc@gmail.com>
2023-12-11 14:04:44 -0300

    Ex 6.4 done

---
+++ Part 6/Ex 6.4/src/App.jsx
@@ -0,0 +1,37 @@
+import { useSelector, useDispatch } from "react-redux";
+
+const App = () => {
+  const dispatch = useDispatch();
+  const anecdotes = useSelector((state) => state);
+
+  const vote = (id) => dispatch({ type: "INCREASE", payload: { id } });
+  const addAnec = (event) => {
+    event.preventDefault();
+    const content = event.target.anecInput.value;
+    dispatch({ type: "NEW_ANEC", payload: { content } });
+  };
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <h2>create new</h2>
+      <form onSubmit={(event) => addAnec(event)}>
+        <div>
+          <input name="anecInput" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
