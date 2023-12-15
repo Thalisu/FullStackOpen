d4535097e5b31437f6002b5e5711e85f2609e810
thalisu <thalisondmcc@gmail.com>
2023-12-15 20:12:52 -0300

    Ex 6.13 done

---
+++ Part 6/Ex 6.13/src/components/AnecdoteForm.jsx
@@ -0,0 +1,37 @@
+import { useDispatch } from "react-redux";
+import { addAnec } from "../reducers/anecdoteReducer";
+import { changeNotification } from "../reducers/notificationReducer";
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const content = event.target.anecInput.value;
+    if (content.length > 4) {
+      dispatch(addAnec(content));
+      dispatch(changeNotification("Anecdote successfully added"));
+      setTimeout(() => {
+        dispatch(changeNotification(""));
+      }, 5000);
+    }
+    dispatch(
+      changeNotification("The length of the anecdote needs to be at least 5")
+    );
+    setTimeout(() => {
+      dispatch(changeNotification(""));
+    }, 5000);
+    return;
+  };
+
+  return (
+    <form onSubmit={(event) => handleSubmit(event)}>
+      <div>
+        <input name="anecInput" />
+      </div>
+      <button type="submit">create</button>
+    </form>
+  );
+};
+
+export default AnecdoteForm;
