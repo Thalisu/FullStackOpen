088010d791258e776242e60c41c8be3ed2b984a2
thalisu <thalisondmcc@gmail.com>
2023-12-26 10:58:48 -0300

    Ex 6.17 done

---
+++ Part 6/Ex 6.17/src/main.jsx
@@ -0,0 +1,20 @@
+import ReactDOM from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import App from "./App";
+import anecReducer from "./reducers/anecdoteReducer";
+import filterReducer from "./reducers/filterReducer";
+import notificationReducer from "./reducers/notificationReducer";
+
+const store = configureStore({
+  reducer: {
+    anec: anecReducer,
+    filter: filterReducer,
+    notification: notificationReducer,
+  },
+});
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
