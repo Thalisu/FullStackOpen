c853604392884268f395ed45c5a9e3ac2086506c
thalisu <thalisondmcc@gmail.com>
2024-01-04 19:42:12 -0300

    Ex 7.3 done

--- Part 7/Ex 7.3/src/reducers/anecdotes/actionCreators.js
+++
@@ -1,9 +0,0 @@
-//import { addAnecdote } from "./slice";
-
-// const vote = (id) => {
-//   return (dispatch) => dispatch(addVote(id));
-// };
-// const createNew = (content, author, info) => {
-//   return addAnecdote();
-// };
-// export default { createNew };
