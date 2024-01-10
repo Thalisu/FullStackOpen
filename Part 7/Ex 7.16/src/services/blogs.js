8747c1c18da32e13f30d6dfd3dd098253e1e6686
thalisu <thalisondmcc@gmail.com>
2024-01-10 12:34:59 -0300

    Ex 7.16 done

---
+++ Part 7/Ex 7.16/src/services/blogs.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3003/blogs";
+
+let token = null;
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`;
+};
+
+const getAll = async () => {
+  const request = await axios.get(baseUrl);
+  const blogs = request.data.sort((a, b) => b.likes - a.likes);
+  return blogs;
+};
+
+const create = async (newObject) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+
+  const response = await axios.post(baseUrl, newObject, config);
+  return response.data;
+};
+
+const deleteBlog = async (id, user) => {
+  const config = {
+    headers: { Authorization: token },
+    data: { user: user },
+  };
+  const response = await axios.delete(`${baseUrl}/${id}`, config);
+  return response.data;
+};
+
+const like = async (blog) => {
+  const { id, ...updatedBlog } = blog;
+  const response = await axios.put(`${baseUrl}/${id}`, updatedBlog);
+  return response.data;
+};
+
+export default { getAll, create, setToken, like, deleteBlog };
