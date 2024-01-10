b7f15551ac69e3517a7de348944554910cccd48e
thalisu <thalisondmcc@gmail.com>
2024-01-10 10:57:16 -0300

    Ex 7.11 done

---
+++ Part 7/Ex 7.11/backend/utils/middleware.js
@@ -0,0 +1,35 @@
+let morgan = require("morgan");
+const logger = require("./logger");
+
+morgan = morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, "content-length"),
+    "-",
+    tokens["response-time"](req, res),
+    "ms",
+    tokens.method(req, res) === "POST" ? JSON.stringify(req.body) : "",
+  ].join(" ");
+});
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(400).json({ error: error.message });
+  } else if (error.name === "TokenExpiredError") {
+    return response.status(401).json({ error: "token expired" });
+  }
+
+  next(error);
+};
+module.exports = {
+  errorHandler,
+  requestLogger: morgan,
+};
