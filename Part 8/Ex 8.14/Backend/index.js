36c3ec84e2cf93fc46eb73d8fa00dca871a5ee31
thalisu <thalisondmcc@gmail.com>
2024-01-26 13:48:55 -0300

    Ex 8.14 done

---
+++ Part 8/Ex 8.14/Backend/index.js
@@ -0,0 +1,113 @@
+const { ApolloServer } = require("@apollo/server");
+const { GraphQLError } = require("graphql");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require("uuid");
+const config = require("./utils/config");
+const mongoose = require("mongoose");
+const Author = require("./models/author");
+const Book = require("./models/book");
+
+mongoose.set("strictQuery", false);
+console.log("connecting to", config.MONGODB_URI);
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connection to MongoDB:", error.message);
+  });
+
+const typeDefs = `
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int!
+    id: ID!
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author: Author!
+    genres: [String!]!
+    id: ID!
+  }
+
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]!
+    allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
+  }
+`;
+
+const resolvers = {
+  Query: {
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (parent, args) => {
+      if (args.author) {
+        const author = await Author.findOne({ name: args.author });
+        return Book.find({ author: author.id }).populate("author");
+      }
+      if (args.genre) {
+        return Book.find({ genres: { $in: [args.genre] } }).populate("author");
+      }
+      return Book.find({}).populate("author");
+    },
+    allAuthors: async () => Author.find({}),
+  },
+
+  Author: {
+    bookCount: async (parent) =>
+      Book.find({ author: parent.id }).countDocuments(),
+  },
+
+  Mutation: {
+    addBook: async (root, args) => {
+      const author = await Author.findOne({ name: args.author });
+      if (!author) {
+        const newAuthor = new Author({ name: args.author });
+        await newAuthor.save();
+        args.author = author;
+        const book = new Book({ ...args });
+        return book.save();
+      }
+      args.author = author.id;
+      console.log(args);
+      const book = new Book({ ...args });
+      return book.save();
+    },
+
+    editAuthor: async (root, args) => {
+      const author = await Author.findOne({ name: args.name });
+      author.born = args.setBornTo;
+      return author.save();
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+}).then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
---
+++ Part 8/Ex 8.14/frontend/queries.js
@@ -0,0 +1,49 @@
+import { gql } from "@apollo/client";
+
+export const ALL_BOOKS = gql`
+  query AllBooks {
+    allBooks {
+      title
+      author
+      published
+    }
+  }
+`;
+export const ALL_AUTHORS = gql`
+  query AllAuthors {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`;
+
+export const CREATE_BOOK = gql`
+  mutation Mutation(
+    $title: String!
+    $author: String!
+    $published: Int!
+    $genres: [String!]!
+  ) {
+    addBook(
+      title: $title
+      author: $author
+      published: $published
+      genres: $genres
+    ) {
+      author
+      published
+      title
+    }
+  }
+`;
+
+export const EDIT_AUTHOR = gql`
+  mutation Mutation($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      name
+      born
+    }
+  }
+`;
