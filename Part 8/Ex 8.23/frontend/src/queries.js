e8a8e55deddcf9fc302f8aa224850d5ab95679ab
thalisu <thalisondmcc@gmail.com>
2024-01-30 13:52:23 -0300

    Ex 8.22 and 8.23 done

---
+++ Part 8/Ex 8.23/frontend/src/queries.js
@@ -0,0 +1,88 @@
+import { gql } from "@apollo/client";
+
+const BOOK_DETAILS = gql`
+  fragment BookDetails on Book {
+    id
+    title
+    genres
+    published
+    author {
+      name
+      born
+      bookCount
+      id
+    }
+  }
+`;
+
+export const BOOK_ADDED = gql`
+  subscription {
+    bookAdded {
+      ...BookDetails
+    }
+  }
+  ${BOOK_DETAILS}
+`;
+
+export const ME = gql`
+  query Me {
+    me {
+      username
+      favoriteGenre
+    }
+  }
+`;
+
+export const ALL_BOOKS = gql`
+  query AllBooks($genre: String) {
+    allBooks(genre: $genre) {
+      ...BookDetails
+    }
+  }
+  ${BOOK_DETAILS}
+`;
+export const ALL_AUTHORS = gql`
+  query AllAuthors {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`;
+
+export const ADD_BOOK = gql`
+  mutation Mutation(
+    $title: String!
+    $author: String!
+    $published: Int!
+    $genres: [String!]!
+  ) {
+    addBook(
+      title: $title
+      author: $author
+      published: $published
+      genres: $genres
+    ) {
+      ...BookDetails
+    }
+  }
+  ${BOOK_DETAILS}
+`;
+
+export const EDIT_AUTHOR = gql`
+  mutation Mutation($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      name
+      born
+    }
+  }
+`;
+
+export const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      value
+    }
+  }
+`;
