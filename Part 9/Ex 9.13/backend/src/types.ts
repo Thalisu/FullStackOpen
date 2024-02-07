edfa4a292dd7c13f6e7c2fcb8648f3869138ba0b
thalisu <thalisondmcc@gmail.com>
2024-02-06 22:35:07 -0300

    Ex 9.13 done

---
+++ Part 9/Ex 9.13/backend/src/types.ts
@@ -0,0 +1,24 @@
+export type Obj = Record<string, unknown>;
+
+export enum Gender {
+  Male = "male",
+  Female = "female",
+  Other = "other",
+}
+
+export interface Diagnosis {
+  code: string;
+  name: string;
+  latin?: string;
+}
+
+export interface Patient {
+  id: string;
+  name: string;
+  occupation: string;
+  gender: Gender;
+  ssn?: string;
+  dateOfBirth?: string;
+}
+
+export type NewPatient = Omit<Patient, "id">;
