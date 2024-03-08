92c769451b0421d4e15d20f08d95d4fd9eea1fea
thalisu <thalisondmcc@gmail.com>
2024-03-07 21:58:09 -0300

    Ex 10.12 done

--- Part 10/part 10.3/App.js
+++ Part 10/part 10.3/App.js
@@ -3,6 +3,7 @@ import { StatusBar } from "expo-status-bar";
 import { NativeRouter } from "react-router-native";
 import { ApolloProvider } from "@apollo/client";
 
+
 import Main from "./src/components/Main";
 import createApolloClient from "./src/utils/apolloClient";
 
