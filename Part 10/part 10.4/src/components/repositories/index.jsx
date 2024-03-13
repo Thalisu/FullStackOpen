f78ba5945a259f1e775e838570ebbb65b386b46b
thalisu <thalisondmcc@gmail.com>
2024-03-13 19:17:58 -0300

    Ex 10.23 done

--- Part 10/part 10.4/src/components/repositories/index.jsx
+++ Part 10/part 10.4/src/components/repositories/index.jsx
@@ -1,10 +1,12 @@
 import { FlatList, View, Text, StyleSheet } from "react-native";
+import { Link } from "react-router-native";
+import { Picker } from "@react-native-picker/picker";
 
 import useRepositories from "../../hooks/useRepositories";
 
 import RepositoryItem from "./RepositoryItem";
 import theme from "../../theme";
-import { Link } from "react-router-native";
+import { useState } from "react";
 
 const styles = StyleSheet.create({
   separator: {
@@ -21,11 +23,29 @@ const styles = StyleSheet.create({
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
-  const { repositories, loading } = useRepositories();
+  const [orderBy, setOrderBy] = useState({
+    orderBy: "RATING_AVERAGE",
+    orderDirection: "DESC",
+  });
+  const { repositories, loading } = useRepositories(orderBy);
+
   if (loading) return <Text>Loading...</Text>;
 
   return (
     <View style={styles.container}>
+      <Picker
+        selectedValue={orderBy}
+        onValueChange={(itemValue) => setOrderBy(itemValue)}
+      >
+        <Picker.Item
+          label="Highest rated repositories"
+          value={{ orderBy: "RATING_AVERAGE", orderDirection: "DESC" }}
+        />
+        <Picker.Item
+          label="Lowest rated repositories"
+          value={{ orderBy: "RATING_AVERAGE", orderDirection: "ASC" }}
+        />
+      </Picker>
       <FlatList
         data={repositories}
         ItemSeparatorComponent={ItemSeparator}
--- Part 10/part 10.4/src/hooks/useRepositories.js
+++ Part 10/part 10.4/src/hooks/useRepositories.js
@@ -2,9 +2,10 @@ import { useQuery } from "@apollo/client";
 
 import { GET_REPOSITORIES } from "../graphql/queries";
 
-const useRepositories = () => {
+const useRepositories = (orderBy) => {
   const { loading, data } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: "cache-and-network",
+    variables: orderBy,
   });
 
   const repositories =
