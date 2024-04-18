98e196f7233c64c632d76acd76815803bacb22bf
thalisu <thalisondmcc@gmail.com>
2024-04-18 18:51:46 -0300

    Ex 13.6 done

---
+++ Part 13/Part 13.2/Ex 13.6/controllers/blogs.js
@@ -0,0 +1,51 @@
+const router = require("express").Router();
+
+const { Blog } = require("../models");
+
+router.get("/", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id);
+  next();
+};
+
+router.get("/:id", blogFinder, async (req, res) => {
+  if (req.blog) {
+    res.json(req.blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.delete("/:id", blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy();
+  }
+  res.status(204).end();
+});
+
+router.put("/:id", blogFinder, async (req, res) => {
+  if (!req.blog || (!req.body.incLike && !req.body.decLike)) {
+    res
+      .status(404)
+      .send(`must inc or dec likes or the blog requested does not exist`)
+      .end();
+  }
+  req.blog.likes += req.body.incLike ? 1 : -1;
+  await req.blog.save();
+  res.json({ likes: req.blog.likes });
+});
+
+module.exports = router;
