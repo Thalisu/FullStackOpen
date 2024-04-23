f8b6ff0ffd5d760eafa84b780dd656b5fc7841d8
thalisu <thalisondmcc@gmail.com>
2024-04-23 19:08:38 -0300

    Ex 13.17 done

---
+++ Part 13/Part 13.3/Ex 13.17/models/user.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require("sequelize");
+
+const { sequelize } = require("../util/db");
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        is: /^[a-z]+$/i,
+        len: [2, 10],
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [2],
+      },
+    },
+    admin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    disabled: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
