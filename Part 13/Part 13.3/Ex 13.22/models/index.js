8ce2626a250c68897a8bdd0d630646c6e24320c5
thalisu <thalisondmcc@gmail.com>
2024-04-24 18:35:59 -0300

    Ex 13.22 done

---
+++ Part 13/Part 13.3/Ex 13.22/models/index.js
@@ -0,0 +1,25 @@
+const Blog = require("./blog");
+const User = require("./user");
+const Team = require("./team");
+const Membership = require("./membership");
+const UserBlogs = require("./user_blogs");
+const ReadingList = require("./reading_list");
+
+User.hasMany(Blog);
+Blog.belongsTo(User);
+
+User.belongsToMany(Team, { through: Membership });
+Team.belongsToMany(User, { through: Membership });
+
+Blog.belongsToMany(Blog, { through: UserBlogs, as: "marked_blogs" });
+Blog.belongsToMany(User, { through: UserBlogs, as: "users_marked" });
+
+Blog.belongsToMany(User, { through: ReadingList, as: "users_reading_list" });
+
+module.exports = {
+  Blog,
+  User,
+  Team,
+  Membership,
+  ReadingList,
+};
